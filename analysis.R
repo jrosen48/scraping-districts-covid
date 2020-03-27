527115ef67a74ea96a0759ae7c97eb932f16509a
Joshua Rosenberg <jmrosen48@gmail.com>
2020-03-27 08:39:59 -0400

    new commits

--- analysis.R
+++ analysis.R
@@ -9,31 +9,34 @@ d <- rename(d, district = "district-name",
             nces_id = "nces-id",
             website_url = "website-url")
 
+d <- d %>% 
+  arrange(state, district)
+
 access_site <- function(name, state, id, url) {
   
   t <- url %>% 
     read_html() %>% 
     html_text()
   
-  candidate_site <- str_detect(tolower(t), "clos*") | str_detect(tolower(t), "corona*") | str_detect(tolower(t), "covid*")
+  closed <- str_detect(tolower(t), "clos*")
+  corona <- str_detect(tolower(t), "corona*")
+  covid <- str_detect(tolower(t), "covid*")
   
-  if (candidate_site) {
-    tibble(district_name = name, state = state, nces_id = id, url = url, candidate_site = TRUE, scraping_failed = FALSE, content = t)
-  } else {
-    tibble(district_name = name, state = state, nces_id = id, url = url, candidate_site = FALSE, scraping_failed = FALSE, content = NA)
-  }
+  tibble(district_name = name, state = state, nces_id = id, url = url, closed = closed, corona = corona, covid = covid, scraping_failed = FALSE, content = t)
+  print(str_c("Processed ", name, " (", state, ") --- closed = ", closed, "; corona = ", corona, "; covid = ", covid))
+  
 }
 
-d <- d[1:5, ]
-
 output <- pmap(list(name = d$district, state = d$state, id = d$nces_id, url = d$website_url), 
-     possibly(access_site, 
-              otherwise = tibble(district_name = NA, 
-                                 state = NA, 
-                                 nces_id = NA, 
-                                 url = NA, 
-                                 candidate_site = FALSE, 
-                                 scraping_failed = TRUE, 
-                                 content = NA)))
+               possibly(access_site, 
+                        otherwise = tibble(district_name = NA, 
+                                           state = NA, 
+                                           nces_id = NA, 
+                                           url = NA, 
+                                           closed = NA,
+                                           corona = NA,
+                                           covid = NA,
+                                           scraping_failed = TRUE, 
+                                           content = NA)))
 
 output <- map_df(output, ~.)
